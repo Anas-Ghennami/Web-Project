2b07fe681f24fe4cfddf3c738dcb0849fe15851e
Anas Ghennami <anisanas829@gmail.com>
2025-12-07 00:47:32 +0100

    sites

---
+++ index.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="index.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+    <title>GCAR</title>
+  </head>    
+</head>
+<body>
+  <header>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+      <div class="container-fluid px-5">
+
+        <a class="navbar-brand fw-bold" href="#" style="color:#EE6E4D; font-family:cursive;">
+          GCAR
+        </a>
+
+
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#gcarNav">
+          <span class="navbar-toggler-icon border"></span>
+        </button>
+
+
+        <div class="collapse navbar-collapse" id="gcarNav">
+          <ul class="navbar-nav ms-auto align-items-lg-center gap-lg-4">
+
+            <li class="nav-item">
+              <a class="nav-link active font-family:cursive;" href="#home">HOME</a>
+            </li>
+
+            <li class="nav-item">
+              <a class="nav-link font-family:cursive;" href="#services">SERVICES</a>
+            </li>
+
+            <li class="nav-item">
+              <a class="nav-link font-family:cursive;" href="#about">ABOUT-US</a>
+            </li>
+
+            <li class="nav-item">
+              <a class="nav-link font-family:cursive;" href="#map">MAP</a>
+            </li>
+
+            <li class="nav-item">
+              <a class="nav-link font-family:cursive;" href="#contact">CONTACT</a>
+            </li>
+            <li class="nav-item ">
+              <a class="btn ms-lg-3" style="color:gray;"  href="#">
+                SIGN IN
+              </a>
+            </li>
+
+            <li class="nav-item ">
+              <a class="btn ms-lg-3 btn-sign"  href="#">
+                SIGN UP
+              </a>
+            </li>
+
+          </ul>
+      </div>
+
+      </div>
+    </nav>
+  </header>
+
+  <section class="sect-after-head hero-section">
+    <div class="container  d-flex align-items-center">
+        <div class="text-white">
+        <h1 class="display-4 fw-bold">GCAR</h1>
+        <p class="lead">Rent your car easily</p>
+        <a class="btn btn-sign btn-lg">Discover Cars</a>
+      </div>
+  </div>
+  </section>
+
+
+</body>
+</html>
